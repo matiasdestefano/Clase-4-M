9468bfbb2461965679070140569e8197439d0364
Matias De Stefano <matias.destefano@mercadolibre.com>
2021-12-21 11:15:22 -0300

    Ejercicio 1 Realizado

---
+++ Ejercicio-1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type errorSalario struct {
+	mensaje string
+}
+
+func main() {
+	salary := 140000
+	err := validarSalario(salary)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	} else {
+		fmt.Println("Debe pagar impuesto")
+	}
+}
+
+func validarSalario(salary int) error {
+	if salary < 150000 {
+		err := errorDeSalario()
+		return err
+	}
+	return nil
+}
+
+func errorDeSalario() error {
+	return &errorSalario{mensaje: "el salario ingresado no alcanza el mínimo imponible"}
+}
+
+func (e *errorSalario) Error() string {
+	return fmt.Sprintf("Error: %s", e.mensaje)
+}
